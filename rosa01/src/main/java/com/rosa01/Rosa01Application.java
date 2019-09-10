c0966f8daa6e88da2891a8141541b372ecce5843
wanghaihong <wanghaihong@huban360.com>
2019-09-10 12:42:51 +0800

    first

---
+++ rosa01/src/main/java/com/rosa01/Rosa01Application.java
@@ -0,0 +1,13 @@
+package com.rosa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rosa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rosa01Application.class, args);
+	}
+
+}
